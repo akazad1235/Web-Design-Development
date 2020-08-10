92efc559252f5f66b92951e6cfc497271de7cc0a
akazad1235 <akazad914@gmail.com>
2020-08-10 19:58:32 -0400

    map, filter, find function

--- ES-6/map.js
+++ ES-6/map.js
@@ -31,3 +31,26 @@ console.log(numbers.map(squares));
 var result = numbers.filter(element => element<4)
 console.log(result);
 
+
+const students = [
+    {id:23, name:'md.azad'},
+    {id:20, name:'md.azad 1'},
+    {id:40, name:'md.azad 2'},
+    {id:45, name:'md.azad 3'},
+]
+
+for (let i = 0; i < students.length; i++) {
+    const element = students[i];
+    
+    console.log(element.id);
+    console.log(element.name);
+}
+
+let studentResult = students.map(x => x.id)
+console.log(studentResult)
+
+let bigger = students.filter(x => x.id >23)
+console.log(bigger);
+let biggerOne = students.find(x => x.id >23)
+console.log(biggerOne);
+
